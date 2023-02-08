d54d0ef0f982e1f1ec1a2b03f75536c0b9f97dd9
Akshay Bobhate <120335788+Akshay-Bob@users.noreply.github.com>
2023-02-08 18:56:06 +0000

    install exe

--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
 
 const app = express();
 
+//create express applications
 app.use(cors());
 app.use(express.json());
 
+//create a get route
 app.get('/message', (req, res) => {
     res.json({ message: "Hello from server!" });
 });
 
+//start server
 app.listen(8000, () => {
     console.log(`Server is running on port 8000.`);
   });
+
+mongoose.connect("")
