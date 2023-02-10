f63cc6abecc5b6b8d39994f7628197de475b137a
Akshay Bobhate <120335788+Akshay-Bob@users.noreply.github.com>
2023-02-10 19:15:56 +0000

    newsletter section

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import Newsletter from "./Footer/Newsletter/Newsletter";
 import Banner from "./Home Banner/Banner";
 import Shopcomponenet from "./Shopcomponent/ShopComponent";
 
 function Home() {
-    return (<Container>
+    return (<>
       <Banner/>
-      <Shopcomponenet/>
-
-    </Container>
+      {/* <Shopcomponenet/> */}
+      <Newsletter/>
+    </>
 )}
 
 export default Home;
