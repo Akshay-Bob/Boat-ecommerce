d5e0a6c9d03b6b3eca241dcdc7a28509e2237aad
Akshay Bobhate <120335788+Akshay-Bob@users.noreply.github.com>
2023-02-14 10:50:07 +0000

    mobile navigation

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -10,12 +10,16 @@ import BoatLogo from '../assests/boat_logo_small.png';
 import SearchComponent from "./SearchComponent";
 import { AiOutlineUser } from "react-icons/ai";
 import { AiOutlineUserAdd } from "react-icons/ai";
-import { Container } from "react-bootstrap";
+// import { Container } from "react-bootstrap";
 
 
 function Header() {
+  function openNav(){
+    let navList = document.querySelector('.mob-header');
+    navList.style.display="block";
+  }
   return (
-  <Container>
+  <>
   <header className="main-header">
     <div className="header-content">
       <div className="left">
@@ -23,7 +27,7 @@ function Header() {
       </div>
 
       <div className="center">
-      <RiMenu2Fill className="mobile-menu" />
+      <RiMenu2Fill className="mobile-menu" onClick={openNav}/>
         <ul>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/about'>About</Link></li>
@@ -44,7 +48,34 @@ function Header() {
         {/* <li><Link to='/signup'><AiOutlineUserAdd/></Link></li> */}
       </div>
     </div>
-  </header></Container>);
+  </header>
+  
+  {/* mobile header */}
+  <header className="mob-header">
+    <div className="mob-content">
+      <div className="center">
+        <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/category/:id'>Categories</Link></li>
+        <li><Link to='/contact'>Contact</Link></li>
+        </ul>
+      </div>
+
+      <div className="right">
+        <TbSearch/>
+        {/* <SearchComponent/> */}
+        <AiOutlineHeart />
+        <span className="cart-icon">
+          <CgShoppingCart />
+          <span>5</span>
+        </span>
+        <li><Link to='/login'><AiOutlineUser/></Link></li>
+        {/* <li><Link to='/signup'><AiOutlineUserAdd/></Link></li> */}
+      </div>
+    </div>
+  </header>
+  </>);
 }
 
 export default Header;
